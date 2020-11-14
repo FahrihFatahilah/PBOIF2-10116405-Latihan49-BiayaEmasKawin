f98c6e64aac3c420a054ef61fd3b5af67d2a50c9
Fahrih Fatahilah <painchikusudo@gmail.com>
2020-11-15 03:08:53 +0800

    Add files via upload

---
+++ PBOIF2-10116405-Latihan49-BiayaEmasKawin/src/Emas/BiayaEmasKawin.java
@@ -0,0 +1,16 @@
+package Emas;
+/**
+ * @author Fahrih Fatahilah
+ * Kelas : IF-2 / PBO-IF2
+ * Nim   : 10116405
+ * Deskripsi Program : Biaya Emas Kawin
+**/
+public class BiayaEmasKawin {
+    public static void main(String[] args) {
+        Emas emas = new Emas(570000,15.7);
+        
+        emas.tampil();
+        
+    }
+    
+}
---
+++ PBOIF2-10116405-Latihan49-BiayaEmasKawin/src/Emas/Emas.java
@@ -0,0 +1,37 @@
+package Emas;
+/**
+ * @author Fahrih Fatahilah
+ * Kelas : IF-2 / PBO-IF2
+ * Nim   : 10116405
+ * Deskripsi Program : Biaya Emas Kawin
+**/
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+public class Emas {
+    private double hargaSatuGram;
+    private double beratEmas;
+    
+    public Emas (double hargaSatuGram, double beratEmas){
+        this.hargaSatuGram = hargaSatuGram;
+        this.beratEmas = beratEmas;
+    }
+    
+    private double totalHarga(){
+        return hargaSatuGram * beratEmas;
+    }
+    
+    public void tampil(){
+        DecimalFormat kursID = (DecimalFormat) DecimalFormat.getCurrencyInstance();
+        DecimalFormatSymbols formatRp = new DecimalFormatSymbols();
+        
+        formatRp.setCurrencySymbol(" Rp. ");
+        formatRp.setMonetaryDecimalSeparator(',');
+        formatRp.setGroupingSeparator('.');
+        kursID.setDecimalFormatSymbols(formatRp);
+        
+        System.out.printf("===Program Pembelian bulan Oktober 2020===\n");
+        System.out.printf("Mas yan dibeli Hendi : %.1f gram\n", beratEmas);
+        System.out.printf("Harga 1 gram : Rp. %s\n", kursID.format(hargaSatuGram));
+        System.out.printf("Total Harga  : Rp. %s\n", kursID.format(totalHarga()));
+    }
+}
